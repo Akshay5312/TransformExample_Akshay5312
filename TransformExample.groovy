c213f34750edb08c29396eb6e5d1a8ae68b2ea26
Kevin Harrington <mad.hephaestus@gmail.com>
2018-04-17 19:58:36 -0400

    comments

--- TransformExample.groovy
+++ TransformExample.groovy
@@ -1,6 +1,7 @@
 //make a list of transforms
 def listOfLocations =[]
 double size =15
+// Simple circle
 for(int i=0;i<15;i++){
 	listOfLocations.add(new Transform()
 		.rot( 0, 0, i*(360/size)) // x,y,z	
@@ -8,13 +9,14 @@ for(int i=0;i<15;i++){
  		.rot( 0, -90, 90) // x,y,z	
  		)
 }
+// Golden ratio spiral
 for(int i=0;i<15;i++){
 	listOfLocations.add(new Transform()
 		.rot( 0, 0, i*(360/size)) // x,y,z	
 		.translate(i*3,0,40)// X , y, z	
  		.rot( 0, -90, 90) // x,y,z	
  		)
-}
+// Add z and get 3d spiral
 for(int i=0;i<15;i++){
 	listOfLocations.add(new Transform()
 		.rot( 0, 0, i*(360/size)) // x,y,z	
