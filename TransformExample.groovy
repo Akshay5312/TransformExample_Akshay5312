de5c50a093d1658394bbd36ea6c800225dec8bef
Kevin Harrington <mad.hephaestus@gmail.com>
2018-07-16 20:30:50 -0400

    simpler transform example

--- TransformExample.groovy
+++ TransformExample.groovy
@@ -1,39 +1,23 @@
-//make a list of transforms
-def listOfLocations =[]
-double size =15
-// Simple circle
-for(int i=0;i<15;i++){
-	listOfLocations.add(new Transform()
-		.rot( 0, 0, i*(360/size)) // x,y,z	
-		.translate(20,0,0)// X , y, z	
- 		.rot( 0, -90, 90) // x,y,z	
- 		)
-}
-// Golden ratio spiral
-for(int i=0;i<15;i++){
-	listOfLocations.add(new Transform()
-		.rot( 0, 0, i*(360/size)) // x,y,z	
-		.translate(i*3,0,40)// X , y, z	
- 		.rot( 0, -90, 90) // x,y,z	
- 		)
-}
-// Add z and get 3d spiral
-for(int i=0;i<15;i++){
-	listOfLocations.add(new Transform()
-		.rot( 0, 0, i*(360/size)) // x,y,z	
-		.translate(i*4,0,80+i*5)// X , y, z	
- 		.rot( 0, -90, 90) // x,y,z	
- 		)
-}
-// make and object we want in many locations
-def object = new Cylinder(	20, // Radius at the bottom
-                      		0, // Radius at the top
-                      		20, // Height
-                      		(int)3 //resolution
-                      		).toCSG()//convert to CSG to display 
-                      		.rotz(-30)
-// apply each transform and collect the result in a new list
-def movedObjects = listOfLocations.collect{object.transformed(it)}
 
+
+def pionter = new Cylinder(	20, // Radius at the bottom
+                      		0, // Radius at the top
+                      		40, // Height
+                      		(int)4 //resolution
+                      		).toCSG()//convert to CSG to display      
+                      		              			 
+def transform = new Transform()
+		.rotz( 10) // x,y,z	
+		.movex(20)// X , y, z	
+ 		.roty( 45) // x,y,z
+def pionterftmoved =  pionter.transformed(   transform)
+// is the same as
+def piontermoved =  pionter
+		.rotz( 10) // x,y,z	
+		.movex(20)// X , y, z	
+ 		.roty( 45) // x,y,z
+			
 // return the list of parts
-return movedObjects
+return [pionterftmoved,
+piontermoved.movez(1)// move it up to see that its even there
+]
